d84d2cfadfaf3750274c54fde95fa975c94b1b03
徐智勇 <bostonhsu@outlook.com>
2018-12-14 10:39:06 +0800

    dynamic add pages in DOM.

--- customecode.js
+++ customecode.js
@@ -1,3 +1,12 @@
 $(document).bind("mobileinit", function() {
-    // my codes here.
+    function addPages() {
+        for (var i = 1; i < 5; i++) {
+            var page = $("<div>").jqmData("role", "page").attr("id", "page" + i);
+            $("<div>").attr("data-role", "header").append($("<h1>").html("Page" + i)).appendTo(page);
+            $("<div>").attr("data-role", "content").append($("<p>").html("Contents for page" + i)).appendTo(page);
+            $("body").append(page);
+            $("<li>").append($("<a>").attr("href", "#page" + i).html("Go to page" + i)).appendTo("#list1");
+        }
+        $("#button1").hide();
+    };
 });
